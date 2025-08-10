efa95507c1f31fcf627ceb75adbdda7ba7d411bb
Vignesh-Ops <vigneshvickymailid@gmail.com>
2025-08-10 10:53:59 -0400

    bug fixes 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,31 +6,31 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 
 function Home() {
-  const {user,status} = useSelector((state) => state.auth);
+//   const {user,status} = useSelector((state) => state.auth);
 
-  // When user data is still loading, show nothing or a loader
- console.log('status',status)
-  if (status=='loading' || status=='pending' || status=='idle' ) {
-    return (
-      <div className="max-w-6xl mx-auto p-6">
-        <div className="animate-pulse">
-        <div className="h-12 bg-gray-200 rounded w-1/3 mb-4"></div>
-<div className="h-6 bg-gray-200 rounded w-2/3 mb-4"></div>
-<div className="h-48 bg-gray-200 rounded"></div>
+//   // When user data is still loading, show nothing or a loader
+//  console.log('status',status)
+//   if (status=='loading' || status=='pending' ) {
+//     return (
+//       <div className="max-w-6xl mx-auto p-6">
+//         <div className="animate-pulse">
+//         <div className="h-12 bg-gray-200 rounded w-1/3 mb-4"></div>
+// <div className="h-6 bg-gray-200 rounded w-2/3 mb-4"></div>
+// <div className="h-48 bg-gray-200 rounded"></div>
 
-        </div>
-      </div>
-    );
-  }
-  // If profile is complete, redirect to bio
-  if (
-    user.username &&
-    user.theme &&
-    Array.isArray(user.bioLinks) &&
-    user.bioLinks.length > 0
-  ) {
-    return <Navigate to="/app/bio" replace />;
-  }
+//         </div>
+//       </div>
+//     );
+//   }
+//   // If profile is complete, redirect to bio
+//   if (
+//     user.username &&
+//     user.theme &&
+//     Array.isArray(user.bioLinks) &&
+//     user.bioLinks.length > 0
+//   ) {
+//     return <Navigate to="/app/bio" replace />;
+//   }
 
   // Otherwise, show landing page
   return (
